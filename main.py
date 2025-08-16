34c564a5aed53e2cd56fa199f1b76e4b442dec08
Sharad Garg <153007263+sharad0x@users.noreply.github.com>
2025-08-16 20:10:14 +0530

    1st cheatcode(adminImmortal) added

--- main.py
+++ main.py
@@ -1,44 +1,64 @@
 import random
+import utils as u
 
-l=["stone", "paper", "scissor"]
+def cheatsCheck(cmd: str, user: int, comp: int) -> tuple[int, int]:
+	"""
+	Checks if the command is an admin command and applies it.
+	"""
+	if cmd.isdigit() and int(cmd) in [1, 2, 3]:
+		return int(cmd), comp # Proceed game normally
+	
+	elif cmd == "adminImmortal":
+		return u.adminImmortal(user, comp) # Apply Immortal command
+	
+	else:
+		return cmd, comp # Invalid command, return unchanged values
+		
 
-##########33333
+def main():
+	# Greetings 
+	print("Welcome to the game of Stone, Paper, Scissor!")
+	print("You can enter your choice as a number (1 for stone, 2 for paper, 3 for scissor) or use commands like 'adminImmortal' to cheat.")
+	options = {1: "stone", 2: "paper", 3: "scissor"}
+	commands = ["adminImmortal", 'fyou']
 
-comp=0
-userr=0
-while comp<5 and userr<5:
-	ch = int(input(f"Enter your choice({l}): "))
-	user = l[ch-1]
-	if user not in l:
-		print(f"choose from these options: {l}")
-		continue
-	a=random.choice(l)
-	print(f"Computer choose: {a}")
-	if user==a:
-		print("Its a tie! ")
-	elif (user=="stone" and a== "scissor"):
-		print("YOU wins !!!!")
-		userr += 1
-	elif user=="stone" and a=="paper":
-		print("computer wins !!!")
-		comp +=1
-	elif user=="paper" and a=="stone":
-		print("YOU wins !!!")
-		userr +=1
-	elif user=="paper" and a=="scissor":
-		print("computer wins !!!!!")
-		comp+=1
-	elif user=="scissor" and a=="paper":
-		print("YOU wins !!!!")
-		userr+=1
-	elif user=="scissor" and a=="stone":
-		print("computer wins !!!!!")
-		comp+=1
-	print(f"computer score: {comp} \nuser score: {userr}")
-if comp==5 : 
-	print("COMPUTER won !!! ")
-else :
-	print("tu jit gya ladle ") 
+	# User and Computer Scores
+	compS=0
+	userS=0
+	# User and Computer choices
+	user = -1
+	comp = -1
+
+	# Main game loop
+	while compS<5 and userS<5:
+		
+		ch = input(f"Enter your choice: ")
+		comp=random.choice(list(options.keys()))
+		user, comp = cheatsCheck(ch, user, comp)
 
 
+		# If choice is not valid(neither a number nor a command) 
+		if user not in [1, 2, 3] and user not in commands:
+			print(f"Choose from these options: {options}")
+			continue
 
+		print(f"Computer's choice: {options[comp]}\tYour choice: {options[user]}")
+		if user==comp:
+			print("Its a tie! ")
+		elif (user==1 and comp== 3) or (user==2 and comp==1) or (user==3 and comp==2):
+			print("You Scored !!!!")
+			userS += 1
+
+		elif (user==3 and comp==1) or (user==2 and comp==3) or (user==1 and comp==2):
+			print("Computer Scored !!!!!")
+			compS+=1
+		print(f"compSuter score: {compS} \t User score: {userS}")
+
+	# Check for game end
+	if compS==5 : 
+		print("Computer WON !!! ")
+	else :
+		print("tu jit gya ladlee !!!") 
+
+if __name__ == "__main__":
+	main()
---
+++ utils.py
@@ -0,0 +1,6 @@
+def adminImmortal(u: int, c: int) -> tuple[int, int]:
+    global userC, compC
+    u = 1
+    c = 3
+    return u, c
+    
